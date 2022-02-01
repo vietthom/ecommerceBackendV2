02964e6e7a6e0933567d46d94ae86a6be0cef274
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-02-01 16:42:51 -0700

    created product model

---
+++ models/Product.js
@@ -0,0 +1,49 @@
+const {Model, DataTypes}= require('sequelize');
+const sequelize= require('../config');
+
+class Product extends Model {}
+
+Product.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        product_name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price:{
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate:{
+                isDecimal: true,
+            },
+        },
+        stock:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate:{
+                isNumeric: true,
+            },
+        }, 
+        catergory_id:{
+            type: DataTypes.INTEGER,
+            references:{
+                model: 'Category',
+                key:'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'product',
+    }
+);
+
+module.exports = Product;
