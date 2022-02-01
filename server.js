9babd3857eb1d27d920b4d19eddf2685528966d5
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-02-01 15:51:43 -0700

    established server and connect to database and port

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const sequelize= require('./config');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+sequelize.sync({force:true}).then(()=>{
+    app.listen(PORT, ()=>console.log(`Server listening on PORT: ${PORT}`));
+});
