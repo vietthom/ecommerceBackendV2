477f711f2072267d92fcd776a80112e9e74b71fe
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-02-01 16:52:39 -0700

    created Tag model

---
+++ models/Tag.js
@@ -0,0 +1,27 @@
+const {Model, DataTypes}= require('sequelize');
+const sequelize = require('../config');
+
+class Tag extends Model {}
+
+Tag.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        tag_name:{
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        ModelName: 'Tag',
+    },
+);
+
+module.exports= Tag;
